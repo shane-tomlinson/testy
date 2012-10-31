9bb930c799686921e956e85f6b2900f60d9df930
Shane Tomlinson <stomlinson@mozilla.com>
2012-10-31 17:23:49 +0000

    It works!

--- server/deployer.js
+++ server/deployer.js
@@ -2,7 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const child_process     = require('child_process');
+const child_process     = require('child_process'),
+      path              = require('path'),
+      fs                = require('fs');
 
 var deployerCmd = function(cmd, args, cwd, cb) {
   // deploy.js does all its work based on the PWD variable.
@@ -30,7 +32,7 @@ exports.create = function(cwd, name, type, cb) {
 };
 
 exports.update = function(cwd, name, cb) {
-  console.log(" >>> Updating aws instance", name);
+  console.log(" >>> updating aws instance", name);
   deployerCmd("update", [name], cwd, cb);
 };
 
@@ -38,4 +40,54 @@ exports.destroy = function(cwd, name, cb) {
   console.log(" >>> destroying aws instance", name);
   deployerCmd("destroy", [name], cwd, cb);
 };
+
+exports.getTestResults = function(cwd, name, cb) {
+  console.log(" >>> getting test results from aws instance", name);
+
+  var resultsPath = path.join(cwd, "results");
+
+  // make the results path if it does not exist.
+  if (!fs.existsSync(resultsPath)) {
+    fs.mkdirSync(resultsPath);
+  }
+
+  process.env['PWD'] = resultsPath;
+  var options = {
+    cwd: resultsPath,
+    env: process.env
+  };
+
+  var cmd = 'scp -o "StrictHostKeyChecking no" app@' + name + ".personatest.org:code/automation-tests/results/*.xml .";
+
+  var p = child_process.exec(cmd, options, function(err, r) {
+    var results = getResultsFromDirectory(resultsPath);
+    cb(null, results);
+  });
+  p.stdout.pipe(process.stdout);
+  p.stderr.pipe(process.stderr);
+};
+
+function getResultsFromDirectory(dir, done) {
+  var buffer = "";
+
+  try {
+    var files = fs.readdirSync(dir);
+    files.forEach(function(file) {
+      var filePath = path.join(dir, file);
+      var stats = fs.lstatSync(filePath);
+      if (stats.isFile()) {
+        if (/\.xml$/.test(file)) {
+          var resultContents = fs.readFileSync(filePath, 'utf8');
+
+          buffer += ("\n" + resultContents);
+        }
+      }
+    });
+  } catch(e) {
+    console.log(e.toString());
+    return;
+  }
+
+  return buffer;
+}
 
--- server/start.js
+++ server/start.js
@@ -9,8 +9,54 @@ const express         = require('express'),
       git             = require('./git'),
       deployer        = require('./deployer');
 
-/*const repoURL = "git://github.com/mozilla/browserid.git"*/
-const repoURL = "file://192.168.1.88/Users/stomlinson/development/browserid";
+const repoURL = "git://github.com/mozilla/browserid.git"
+/*const repoURL
+ * = "file://192.168.1.88/Users/stomlinson/development/browserid";*/
+
+function checkErr(state, err, res) {
+  if (err) {
+    console.error(err);
+    teardown(state, function() {
+      res.send(500, err);
+      process.exit(1);
+    });
+  }
+}
+
+var statusUpdateTimeout;
+function teardown(state, cb) {
+  if (statusUpdateTimeout) {
+    clearTimeout(statusUpdateTimeout);
+    statusUpdateTimeout = null;
+  }
+
+  if (state.instance_to_remove) {
+    deployer.destroy(state.dir_to_remove, state.instance_to_remove, function(err, r) {
+      fs.rmdir(state.dir_to_remove, cb);
+    });
+  } else if (state.dir_to_remove) {
+    fs.rmdir(state.dir_to_remove, cb);
+  }
+}
+
+// write out occasional updates so the HTTP connection is not closed.
+function sendUpdate(res, update) {
+  res.write(update + "\n");
+
+  // clear the old timeouts
+  if (statusUpdateTimeout) {
+    clearTimeout(statusUpdateTimeout);
+    statusUpdateTimeout = null;
+  }
+
+  function sendWaitingUpdate() {
+    res.write("   ... waiting ...\n");
+    statusUpdateTimeout = setTimeout(sendWaitingUpdate, 20000);
+  }
+
+  statusUpdateTimeout = setTimeout(sendWaitingUpdate, 20000);
+}
+
 
 var app = express();
 
@@ -28,6 +74,18 @@ app.get('/:sha', function(req, res, next) {
   var sha = req.params.sha;
   console.log(sha);
 
+  // get the instance name, append a random number onto the end to help avoid
+  // collisions
+  var aws_instance_name = "testy-" + sha.substr(0, 8) + "-" + Math.floor(10000 * Math.random());
+
+  // since multiple tests can be running at the same time, keep a small state
+  // momento that we can use to keep track of which directory/instance to
+  // remove when teardown occurs.
+  var state = {
+    dir_to_remove: undefined,
+    instance_to_remove: undefined
+  };
+
   // process:
   // 1) make a temp directory to clone repo into
   // 2) clone the repo
@@ -38,17 +96,50 @@ app.get('/:sha', function(req, res, next) {
   // 7) get the results
   // 8) delete the ephemeral instance
   // 9) remove the temporary directory
+  res.writeHead(200, {
+    'Content-Type': 'text/plain; charset=utf-8'
+  });
+
   temp.mkdir(null, function(err, dirPath) {
+    checkErr(state, err, res);
+
+    sendUpdate(res, " >>> temporary directory created");
+
+    state.dir_to_remove = dirPath;
+
     git.clone(dirPath, repoURL, function(err, r) {
+      checkErr(state, err, res);
+
+      sendUpdate(res, " >>> successful clone of repo: " + repoURL + ", checking out code");
+
       git.checkout(dirPath, sha, "branch_to_test", function(err, r) {
+        checkErr(state, err, res);
+
+        sendUpdate(res, " >>> checked out: " + sha + ", installing dependencies");
+
         git.install_deps(dirPath, function(err, r) {
-          var aws_instance_name = "tester-" + sha.substr(0, 8);
+          checkErr(state, err, res);
+
+          sendUpdate(res, " >>> dependencies installed, creating AWS instance");
+
           deployer.create(dirPath, aws_instance_name, "c1.medium", function(err, r) {
+            checkErr(state, err, res);
+
+            sendUpdate(res, " >>> AWS instance created, pushing code & running tests");
+            state.instance_to_remove = aws_instance_name;
+
             deployer.update(dirPath, aws_instance_name, function(err, r) {
-              deployer.destroy(dirPath, aws_instance_name, function(err, r) {
-                fs.rmdir(dirPath, function(err, r) {
-                  res.send(200, req.param.sha);
-                });
+              checkErr(state, err, res);
+
+              sendUpdate(res, " >>> tests run, fetching results");
+              deployer.getTestResults(dirPath, aws_instance_name, function(err, r) {
+                checkErr(state, err, res);
+
+                sendUpdate(res, " >>> results fetched:");
+                res.write(r);
+                res.end();
+
+                teardown(state);
               });
             })
           });
@@ -58,4 +149,5 @@ app.get('/:sha', function(req, res, next) {
   });
 });
 
-app.listen(3000);
+var port = process.env['NODE_ENV'] === "aws" ? 80 : 3000;
+app.listen(port);
