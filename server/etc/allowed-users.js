e1903c634a0d4152d961df19f0128dad0fee9988
Shane Tomlinson <stomlinson@mozilla.com>
2012-11-05 18:18:03 +0000

    Add a whitelist of allowed users who can start testy deployments.

---
+++ server/etc/allowed-users.js
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+exports.allowed_users = [
+  '6a68',
+  'benadida',
+  'callahad',
+  'fmarier',
+  'jrgm',
+  'lloyd',
+  'mozilla',
+  'ozten',
+  'seanmonstar',
+  'shane-tomlinson',
+  'stomlinson',
+  'warner',
+  'zaach'
+];
+
--- server/start.js
+++ server/start.js
@@ -4,9 +4,11 @@
 
 const express         = require('express'),
       path            = require('path'),
+      url             = require('url'),
       wrench          = require('wrench'),
       git             = require('./git'),
-      deployer        = require('./deployer');
+      deployer        = require('./deployer'),
+      allowed_users   = require('./etc/allowed-users').allowed_users;
 
 const DEFAULT_REPO_URL = "git://github.com/mozilla/browserid.git";
 
@@ -15,7 +17,12 @@ var testsBeingRun = [];
 function checkErr(state, err, res) {
   if (err) {
     console.error(err);
-    res.write(" >>> Aborting: " + state.sha + " - error code: " + String(err));
+    var msg = " >>> Aborting";
+
+    if (state && state.sha) msg += ": " + state.sha;
+    msg += " - error code: " + String(err);
+
+    res.write(msg);
     res.end();
     teardown(state);
 
@@ -34,17 +41,19 @@ function teardown(state, cb) {
     statusUpdateTimeout = null;
   }
 
-  var index = testsBeingRun.indexOf(state);
-  if (index > -1) {
-    testsBeingRun.splice(index, 1);
-  }
+  if (state) {
+    var index = testsBeingRun.indexOf(state);
+    if (index > -1) {
+      testsBeingRun.splice(index, 1);
+    }
 
-  if (state.instance_to_remove) {
-    deployer.destroy(state.dir_to_remove, state.instance_to_remove, function(err, r) {
-      wrench.rmdirRecursive(state.dir_to_remove, done);
-    });
-  } else if (state.dir_to_remove) {
-    wrench.rmdirRecursive(state.dir_to_remove, done);
+    if (state.instance_to_remove) {
+      deployer.destroy(state.dir_to_remove, state.instance_to_remove, function(err, r) {
+        rmTempDir(state.dir_to_remove, done);
+      });
+    } else if (state.dir_to_remove) {
+      rmTempDir(state.dir_to_remove, done);
+    }
   }
 }
 
@@ -85,90 +94,100 @@ app.get('/', function(req, res, next) {
   });
 });
 
-app.get('/test/:sha', function(req, res, next) {
-  var sha = req.params.sha;
-  var repoURL = /*req.query.repo || */DEFAULT_REPO_URL;
+app.post('/test', function(req, res, next) {
+  var sha = req.body.sha;
+  var repoURL = req.body.repo || DEFAULT_REPO_URL;
 
-  console.log("test started by:", req.connection.remoteAddress);
-  console.log(repoURL, sha);
+  console.log("attempting:", repoURL, sha, req.connection.remoteAddress);
 
-  // get the instance name, append a random number onto the end to help avoid
-  // collisions
-  var aws_instance_name = "testy-" + sha.substr(0, 7) + "-" + Math.floor(10000 * Math.random());
+  if (!sha) {
+    checkErr(undefined, new Error("sha must be specified"), res);
+    return;
+  }
 
-  // since multiple tests can be running at the same time, keep a small state
-  // momento that we can use to keep track of which directory/instance to
-  // remove when teardown occurs.
-  var state = {
-    dir_to_remove: undefined,
-    instance_to_remove: undefined,
-    sha: sha,
-    repo: repoURL,
-    // If this is a github repo, prettify the printed URL.
-    github_repo_url: repoURL.indexOf("github.com") > -1 ? repoURL.replace(/^git:/, 'https:').replace(/\.git$/, '') : null
-  };
+  checkRepoAllowed(repoURL, res, function(err, allowed) {
+    if (!allowed) {
+      return;
+    }
 
-  testsBeingRun.push(state);
+    // get the instance name, append a random number onto the end to help avoid
+    // collisions
+    var aws_instance_name = "testy-" + sha.substr(0, 7) + "-" + Math.floor(10000 * Math.random());
 
-  // process:
-  // 1) make a temp directory to clone repo into
-  // 2) clone the repo
-  // 3) check out the correct sha
-  // 4) install the deps
-  // 5) create an ephemeral instance
-  // 6) update the ephemeral instance which cause the tests to run
-  // 7) get the results
-  // 8) delete the ephemeral instance
-  // 9) remove the temporary directory
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
+    // since multiple tests can be running at the same time, keep a small state
+    // momento that we can use to keep track of which directory/instance to
+    // remove when teardown occurs.
+    var state = {
+      dir_to_remove: undefined,
+      instance_to_remove: undefined,
+      sha: sha,
+      repo: repoURL,
+      // If this is a github repo, prettify the printed URL.
+      github_repo_url: repoURL.indexOf("github.com") > -1 ? repoURL.replace(/^git:/, 'https:').replace(/\.git$/, '') : null
+    };
 
-  makeTempDir(aws_instance_name, function(err, dirPath) {
-    if(checkErr(state, err, res)) return;
+    testsBeingRun.push(state);
 
-    sendUpdate(res, " >>> temporary directory created, cloning repo");
+    // process:
+    // 1) make a temp directory to clone repo into
+    // 2) clone the repo
+    // 3) check out the correct sha
+    // 4) install the deps
+    // 5) create an ephemeral instance
+    // 6) update the ephemeral instance which cause the tests to run
+    // 7) get the results
+    // 8) delete the ephemeral instance
+    // 9) remove the temporary directory
+    res.writeHead(200, {
+      'Content-Type': 'text/plain; charset=utf-8'
+    });
 
-    state.dir_to_remove = dirPath;
-
-    git.clone(dirPath, repoURL, function(err, r) {
+    makeTempDir(aws_instance_name, function(err, dirPath) {
       if(checkErr(state, err, res)) return;
 
-      sendUpdate(res, " >>> successful clone of repo: " + repoURL + ", checking out code");
+      sendUpdate(res, " >>> temporary directory created, cloning repo");
 
-      git.checkout(dirPath, sha, "branch_to_test", function(err, r) {
+      state.dir_to_remove = dirPath;
+
+      git.clone(dirPath, repoURL, function(err, r) {
         if(checkErr(state, err, res)) return;
 
-        sendUpdate(res, " >>> checked out: " + sha + ", installing dependencies");
+        sendUpdate(res, " >>> successful clone of repo: " + repoURL + ", checking out code");
 
-        git.install_deps(dirPath, "awsbox", function(err, r) {
+        git.checkout(dirPath, sha, "branch_to_test", function(err, r) {
           if(checkErr(state, err, res)) return;
 
-          git.install_deps(dirPath, "temp", function(err, r) {
+          sendUpdate(res, " >>> checked out: " + sha + ", installing dependencies");
+
+          git.install_deps(dirPath, "awsbox", function(err, r) {
             if(checkErr(state, err, res)) return;
 
-            sendUpdate(res, " >>> dependencies installed, creating AWS instance");
-
-            deployer.create(dirPath, aws_instance_name, "c1.medium", function(err, r) {
+            git.install_deps(dirPath, "temp", function(err, r) {
               if(checkErr(state, err, res)) return;
 
-              sendUpdate(res, " >>> AWS instance created, pushing code & running tests");
-              state.instance_to_remove = aws_instance_name;
+              sendUpdate(res, " >>> dependencies installed, creating AWS instance");
 
-              deployer.update(dirPath, aws_instance_name, function(err, r) {
+              deployer.create(dirPath, aws_instance_name, "c1.medium", function(err, r) {
                 if(checkErr(state, err, res)) return;
 
-                sendUpdate(res, " >>> tests run, fetching results");
-                deployer.getTestResults(dirPath, aws_instance_name, function(err, r) {
+                sendUpdate(res, " >>> AWS instance created, pushing code & running tests");
+                state.instance_to_remove = aws_instance_name;
+
+                deployer.update(dirPath, aws_instance_name, function(err, r) {
                   if(checkErr(state, err, res)) return;
 
-                  sendUpdate(res, " >>> results fetched:");
-                  res.write(r);
-                  res.end();
+                  sendUpdate(res, " >>> tests run, fetching results");
+                  deployer.getTestResults(dirPath, aws_instance_name, function(err, r) {
+                    if(checkErr(state, err, res)) return;
 
-                  teardown(state);
-                });
-              })
+                    sendUpdate(res, " >>> results fetched:");
+                    res.write(r);
+                    res.end();
+
+                    teardown(state);
+                  });
+                })
+              });
             });
           });
         });
@@ -189,6 +208,26 @@ function makeTempDir(tempDirName, done) {
   }
 
   done(err, tempDirPath);
+}
+
+function rmTempDir(tempDirName, done) {
+  wrench.rmdirRecursive(tempDirName, done);
+}
+
+function checkRepoAllowed(repoURL, res, done) {
+  var parsedURL = url.parse(repoURL);
+  var repoOwner = parsedURL.path.split("/")[1];
+
+  var allowed = allowed_users.indexOf(repoOwner) > -1;
+
+  if (!allowed) {
+    var msg = repoOwner + " does not have permission to use testy";
+
+    console.log(msg);
+    res.send(401, msg);
+  }
+
+  done && done(null, allowed);
 }
 
 var port = process.env['PORT'] || 3000;
